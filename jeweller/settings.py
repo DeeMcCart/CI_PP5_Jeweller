0d22cad2a2d3f522960e84b735d4bbd14ae9937f
Deirdre McCarthy <deirdremc1968@gmail.com>
2024-01-23 00:59:07 +0000

    build: Create home app and perform initial configuration Issue #2

--- jeweller/settings.py
+++ jeweller/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'django.contrib.sites',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -80,7 +81,8 @@ ROOT_URLCONF = 'jeweller.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+                 os.path.join(BASE_DIR, 'templates', 'allauth')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
