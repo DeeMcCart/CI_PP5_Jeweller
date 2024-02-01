2f8424a5faaa9c5ee4ec2ea2bb2ac8ee347cb8f6
Deirdre McCarthy <deirdremc1968@gmail.com>
2024-02-01 21:09:09 +0000

    feat: Update order line model with price and qty Issue #17

--- checkout/models.py
+++ checkout/models.py
@@ -72,6 +72,8 @@ class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     line_number = models.IntegerField(default=10) 
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     category = models.CharField(max_length=30, null=True, blank=True)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
