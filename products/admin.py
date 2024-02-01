b164b8935cb1ac2428ce59af967fd1985153072c
Deirdre McCarthy <deirdremc1968@gmail.com>
2024-02-01 13:32:30 +0000

    feat: Add toast html message content Issue #27

--- products/admin.py
+++ products/admin.py
@@ -33,14 +33,4 @@ class ProductAdmin(admin.ModelAdmin):
 
 
 
-admin.site.register(Product)
-admin.site.register(Category)
-admin.site.register(Catname)
-admin.site.register(Cat0)
-admin.site.register(Cat1)
-admin.site.register(Cat2)
-admin.site.register(Cat3)
-admin.site.register(Cat4)
-admin.site.register(Cat5)
-admin.site.register(Cat6)
-
+admin.site.register(Product, Category, Catname, Cat0, Cat1, Cat2, Cat3, Cat4, Cat5, Cat6)
