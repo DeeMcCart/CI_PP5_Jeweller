f472f11c84c129c8046fbdb8a77010e146bccef7
Deirdre McCarthy <deirdremc1968@gmail.com>
2024-02-16 02:15:05 +0000

    build: Add email processing capability #56

--- jeweller/settings.py
+++ jeweller/settings.py
@@ -49,7 +49,7 @@ SITE_ID=1
 ACCOUNT_FORMS = {
 'signup': 'jeweller.forms.CustomSignupForm',
 }
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 # DMcC 05/02/24 Think ACCOUNT_VERIRFICAION_METHOD now replaced by 
 ACCOUNT_VERIFICATION_METHOD="username_email"
 # DMcC ACCOUNT_AUTHENTICATION_METHOD:  specifies login method to use -userame/email/either (AllAuth 0.60.0)
@@ -181,6 +181,7 @@ else:
            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
        }
    }
+print(f'Database setting is ', DATABASES)
 
 
 # Password validation
@@ -280,10 +281,22 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STANDARD_DELIVERY_AMOUNT = 7
 FIRST_ORDER_NUMBER = 24004
 FIRST_LINE_NUMBER = 10
-DEFAULT_FROM_EMAIL='allskills.tech@gmail.com'
+
 
 STRIPE_CURRENCY = 'EUR'
 
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY", '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'Jewellery@example.com'
+else:
+    EMAIL_BACKEND = django.core.mail.backends.smtp.EmailBackend
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = smtp.gmail.com
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
